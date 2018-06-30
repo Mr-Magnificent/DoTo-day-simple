d1bd08dcb89fd1e3e24d1da7b052418a1a5386d3
Ayush Poddar <ayush.zombiestar@gmail.com>
2018-07-01 00:04:03 +0530

    Final Todo list working with delete button

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
 let btn = document.getElementById('btn');
+let deleteElement;
+let buttonDeleteIcon;
+
 
 btn.onclick = function () {
 
@@ -6,20 +9,45 @@ btn.onclick = function () {
         let input = document.getElementById('input');
         let list = document.getElementById('list');
         input = input.value;
+
+        // checks for empty string when btn is pressed
         if (input == '') {
             return;
         }
+
+        //sets the input box to empty string after extracting it's value
         document.getElementById('input').value = '';
-        console.log (input);
+
+        // creates a new li element
         let listItem = document.createElement('li');
         listItem.style.marginTop = '10px';
-        let buttonDeleteIcon = document.createElement('button');
+
+        // creates a new button element
+        buttonDeleteIcon = document.createElement('button');
+
+        // button with a class name as deleteElement
         buttonDeleteIcon.className = 'deleteElement';
+        buttonDeleteIcon.setAttribute('onclick', 'deleteItem(this)');
         buttonDeleteIcon.innerHTML = "<img src='baseline-delete-24px.svg' style='height: 16px' alt='deleteIcon'>";
-        // listItem.appendChild(imgDelIcon);
         listItem.innerHTML = `${input}&nbsp;&nbsp;${buttonDeleteIcon.outerHTML}`;
         list.appendChild(listItem);
+
+        //updates the deleteElement variable after the addition of new lo element
+        deleteElement = document.getElementsByClassName('deleteElement');
+        // console.log(deleteElement);
     }
     addElement();
 };
 
+// deleteElement = document.getElementsByClassName('deleteElement');
+
+// Test function that prints the parent node of the button ( i.e prints the called li element)
+// This is not called when the button is clicked
+function deleteItem(deleteElement) {
+    console.log (deleteElement);
+    function delListItem () {
+        let parentNode = deleteElement.parentNode;
+        parentNode.remove();
+    }
+    delListItem();
+};
