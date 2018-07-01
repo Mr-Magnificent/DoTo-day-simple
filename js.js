efbb43fa1c2a83c7688cbaa0324de829506684e5
Ayush Poddar <ayush.zombiestar@gmail.com>
2018-07-01 23:59:40 +0530

    JavaScript complete with checkbox also working

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ let buttonDeleteIcon;
 btn.onclick = function () {
 
     function addElement() {
-        let input = document.getElementById('input');
+        let input = document.getElementById('inp');
         let list = document.getElementById('list');
         input = input.value;
 
@@ -16,7 +16,7 @@ btn.onclick = function () {
         }
 
         //sets the input box to empty string after extracting it's value
-        document.getElementById('input').value = '';
+        document.getElementById('inp').value = '';
 
         // creates a new li element
         let listItem = document.createElement('li');
@@ -25,29 +25,50 @@ btn.onclick = function () {
         // creates a new button element
         buttonDeleteIcon = document.createElement('button');
 
-        // button with a class name as deleteElement
-        buttonDeleteIcon.className = 'deleteElement';
+        // create a new checkbox element
+        let checkboxMultiple = document.createElement('input');
+        checkboxMultiple.setAttribute('type', 'checkbox');
+        checkboxMultiple.setAttribute('name', 'sel');
+        checkboxMultiple.setAttribute('onchange', 'addToDelList(this)');
+
+        // set the onclick attrib to call deleteItem() with 'this' node
         buttonDeleteIcon.setAttribute('onclick', 'deleteItem(this)');
         buttonDeleteIcon.innerHTML = "<img src='baseline-delete-24px.svg' style='height: 16px' alt='deleteIcon'>";
-        listItem.innerHTML = `${input}&nbsp;&nbsp;${buttonDeleteIcon.outerHTML}`;
+
+        // change inner of li element and append to ul "list"
+        listItem.innerHTML = `${checkboxMultiple.outerHTML}&nbsp;&nbsp;${input}&nbsp;&nbsp;${buttonDeleteIcon.outerHTML}`;
         list.appendChild(listItem);
 
-        //updates the deleteElement variable after the addition of new lo element
-        deleteElement = document.getElementsByClassName('deleteElement');
-        // console.log(deleteElement);
+        //updates the deleteElement variable after the addition of new li element
+        // deleteElement = document.getElementsByClassName('deleteElement');
     }
     addElement();
 };
 
-// deleteElement = document.getElementsByClassName('deleteElement');
-
-// Test function that prints the parent node of the button ( i.e prints the called li element)
-// This is not called when the button is clicked
 function deleteItem(deleteElement) {
-    console.log (deleteElement);
-    function delListItem () {
-        let parentNode = deleteElement.parentNode;
-        parentNode.remove();
+    // console.log (deleteElement);
+    let parentNode = deleteElement.parentNode;
+    parentNode.remove();
+}
+
+let delList = [];
+let ind;
+function addToDelList(obj) {
+    let parentNode = obj.parentNode;
+    ind = delList.indexOf(parentNode);
+    console.log(ind);
+    console.log(delList);
+    if (ind == -1) {
+        delList.push(parentNode);
     }
-    delListItem();
-};
+    else {
+        delList.splice(ind, 1);
+    }
+    console.log(delList);
+}
+
+function delSelectedList() {
+    for(let i = 0; i < delList.length; i++) {
+        delList[i].remove();
+    }
+}
